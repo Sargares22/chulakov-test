9211a915362d3042aab0aa175802778cde9c5f87
Sargares22 <seredkinsp@mail.ru>
2020-06-14 23:48:01 +0700

    Updates

---
+++ precache-manifest.c1f135da58fae5a26bf42ce7872e9273.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72e990bb5066884638d4ef0ccb2d5f1",
+    "url": "/chulakov-test/index.html"
+  },
+  {
+    "revision": "a59f8ae952b11eb1395b",
+    "url": "/chulakov-test/static/css/main.50101c92.chunk.css"
+  },
+  {
+    "revision": "6f5c8e83002020d196b7",
+    "url": "/chulakov-test/static/js/2.e1e35f32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chulakov-test/static/js/2.e1e35f32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59f8ae952b11eb1395b",
+    "url": "/chulakov-test/static/js/main.efd3f86a.chunk.js"
+  },
+  {
+    "revision": "bcec2393a6fb55daac89",
+    "url": "/chulakov-test/static/js/runtime-main.1def0847.js"
+  }
+]);
